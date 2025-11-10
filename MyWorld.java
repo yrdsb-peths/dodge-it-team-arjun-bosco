5d9c6a3024052b4745d460e12aa171ccefb43031
Minigopal <archer.arjunraj@gmail.com>
2025-11-10 14:32:14 -0500

    many edits
    - added 4 more balloon instances
    - added an offset so the balloons slowly spawn in
    - added an explosion for when the balloon touches the player

--- MyWorld.java
+++ MyWorld.java
@@ -4,10 +4,23 @@ public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
         Hero hero = new Hero();
-        RedBalloon rb = new RedBalloon();
-        
+        RedBalloon rb1 = new RedBalloon();
+        RedBalloon rb2 = new RedBalloon();
+        RedBalloon rb3 = new RedBalloon();
+        RedBalloon rb4 = new RedBalloon();
+        RedBalloon rb5 = new RedBalloon();
         addObject(hero, 100, 100);
-        addObject(rb, 0, 0);
         
+        addObject(rb1, 0, 0);
+        addObject(rb2, 0, 0);
+        addObject(rb3, 0, 0);
+        addObject(rb4, 0, 0);
+        addObject(rb5, 0, 0);
+        
+        rb1.sleepFor(10 * (1 + Greenfoot.getRandomNumber(5)));
+        rb2.sleepFor(50 * (3 + Greenfoot.getRandomNumber(5)));
+        rb3.sleepFor(50 * (5 + Greenfoot.getRandomNumber(5)));
+        rb4.sleepFor(50 * (7 + Greenfoot.getRandomNumber(5)));
+        rb5.sleepFor(50 * (9 + Greenfoot.getRandomNumber(5)));
     }
 }
--- RedBalloon.java
+++ RedBalloon.java
@@ -12,18 +12,37 @@ public class RedBalloon extends Actor
      * Act - do whatever the RedBalloon wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    int dy = -1;
-    int startingXPos = 10 + Greenfoot.getRandomNumber(590);
+    int dy = -1 * (1 + Greenfoot.getRandomNumber(2));
+    int startingXPos = 10 + Greenfoot.getRandomNumber(591);
+    
     public void act()
     {
         // Add your action code here.
         if (getY() < 1)
         {
-            startingXPos = 50 + Greenfoot.getRandomNumber(500);
+            dy = -1 * (1 + Greenfoot.getRandomNumber(2));
+            
+            startingXPos = 10 + Greenfoot.getRandomNumber(591);
             setLocation(startingXPos, 600);
+            if (isTouching(RedBalloon.class))
+            {
+                startingXPos = 10 + Greenfoot.getRandomNumber(591);
+                setLocation(startingXPos, 600);
+            }
         }
         
         setLocation(startingXPos, getY() + dy);
+        
+        if (isTouching(Hero.class))
+        {
+            Explode boom = new Explode();
+            getWorld().addObject(boom, getX(), getY());
+            getWorld().removeObject(this);
+        }
+        
+        
+        
+        
         
         
     }
